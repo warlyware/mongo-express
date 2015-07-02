1eb687ad73b8873acba4572a211d8eb389719f06
warlyware <danward@gmail.com>
2015-07-02 11:37:38 -0700

    Add POST method for questions

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,14 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/test');
+mongoose.connect(process.env.MONGO_URL);
+
+var Question = mongoose.model('Question', {
+  body: String,
+  email: String,
+  createdAt: Date
+});
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -13,10 +20,25 @@ router.get('/test', function(req, res, next) {
   res.send('Just testing');
 });
 
+router.post('/questions', function(req, res) {
+  var question = new Question(req.body);
+  question.createdAt = new Date();
+  question.save(function(err, savedQuestion) {
+    res.send(savedQuestion);
+  });
+});
+
 router.post('/test', function(req, res, next) {
   var response = req.body;  
   response.serverTime = new Date();  //Add serverTime property to response obj
   res.json(response);
 });
+
+// router.get('/users', function(req, res, next) {
+//   User.find({}, function(err, data) {
+//     console.log(data);
+//     res.json(data);
+//   });
+// });
 
 module.exports = router;
